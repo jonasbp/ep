64c5259e23d0acdfed42784944e8612325819436
Jonas Bonfá <jonasbonfapelegrina@gmail.com>
2020-10-16 18:01:03 -0300

    Teste de importação de funções

--- funções.py
+++ funções.py
@@ -1,3 +1,8 @@
 # EP - Design de Software
 # Equipe: Jonas Bonfá Pelegrina e Layne Pereira da Silva
 # Data: 16/10/2020
+
+#Função para testar o import do arquvo principal
+def teste(x):
+    print("O número é")
+    return(x)
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # EP - Design de Software
 # Equipe: Jonas Bonfá Pelegrina e Layne Pereira da Silva
 # Data: 16/10/2020
+
+#Importando as funções do arquivo funções.py
+import funções as back
+
+print(back.teste(1))
